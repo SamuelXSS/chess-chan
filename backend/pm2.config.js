a2aa9a0100d1374c91d3e5e73904ffd3ca786960
SamuelXSS <samuelxses@unipam.edu.br>
2023-08-12 19:42:29 -0300

    :sparkles: pm2 config file

---
+++ backend/pm2.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'chesschan-api-dev',
+      script: 'src/server.js',
+      env: {
+        NODE_ENV: 'dev',
+      },
+    },
+    {
+      name: 'chesschan-api-prod',
+      script: 'src/server.js',
+      env: {
+        NODE_ENV: 'prod',
+      },
+    },
+  ],
+};
